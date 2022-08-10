2eb74569d105a230df405322b0530f0869791c31
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-08-10 15:00:46 -0700

    104. Maximum Depth of Binary Tree

---
+++ Everything_ Data_Structures_And_Algorithms/Blind75_By_NeetCode/7_Trees/104_Maximum_Depth_of_Binary_Tree/recursive_method.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/* Recursive method, Time: O(n), Space: O(n) */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        int maxDepth = 0;
+
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+
+    }
+}
