49a0126b07493dfa56a6d6641989fe8630fec923
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-08-15 01:55:24 -0700

    206. Reverse Linked List

---
+++ Everything_ Data_Structures_And_Algorithms/Blind75_By_NeetCode/6_Linked_List/206_Reverse_Linked_List/iterative_method.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/* Time: O(n), Space: O(1) */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode nextTemp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nextTemp;
+        }
+
+        return prev;
+    }
+}
