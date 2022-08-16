4f0a0080a2f4a8bcf2ad5a63047c5e8dc59e7bda
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-08-16 16:19:38 -0700

    206. Reverse Linked List

--- Everything_ Data_Structures_And_Algorithms/Blind75_By_NeetCode/6_Linked_List/206_Reverse_Linked_List/recursive_method.java
+++ Everything_ Data_Structures_And_Algorithms/Blind75_By_NeetCode/6_Linked_List/206_Reverse_Linked_List/recursive_method.java
@@ -9,15 +9,21 @@
  * }
  */
 
-/* Time: O(n), Space: O(n) */
+/* 206. Reverse Linked List */
+/* Recursive method, Time: O(n), Space: O(n) */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        // Base case: if head input is null, or head.next is null then return the head back to previous call stack
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode node = reverseList(head.next);
+        // used for storing the head of the last node from original input
+        ListNode reversedHead = reverseList(head.next);
+        // set curr node's next node's next pointer to curr node
         head.next.next = head;
+        // set curr node's next pointer to null
         head.next = null;
-        return node;
+        // return the reversed head node back to previous call stack
+        return reversedHead;
     }
 }
