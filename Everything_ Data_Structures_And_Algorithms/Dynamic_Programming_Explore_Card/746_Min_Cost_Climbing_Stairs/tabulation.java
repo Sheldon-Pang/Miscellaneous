0fd730e9801d21c8d39941d8cb259c893f3337f0
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-08-23 20:15:02 -0700

    746. Min Cost Climbing Stairs

---
+++ Everything_ Data_Structures_And_Algorithms/Dynamic_Programming_Explore_Card/746_Min_Cost_Climbing_Stairs/tabulation.java
@@ -0,0 +1,16 @@
+/* 746. Min Cost Climbing Stairs */
+/* Tabulation, Time: O(n), Space: O(n) */
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+        int minimumCost[] = new int[cost.length + 1];
+
+        for (int i = 2; i < minimumCost.length; i++) {
+            int takeOneStep = minimumCost[i - 1] + cost[i - 1];
+            int takeTwoSteps = minimumCost[i - 2] + cost[i - 2];
+            minimumCost[i] = Math.min(takeOneStep, takeTwoSteps);
+        }
+
+        return minimumCost[minimumCost.length - 1];
+    }
+}
