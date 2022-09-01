990e597a61b966fa7451c72a8863b4431fe94ae2
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-08-31 17:42:57 -0700

    62. Unique Paths

---
+++ Everything_ Data_Structures_And_Algorithms/Recursion_Explore_Card/62_Unique_Paths/recursive_method.java
@@ -0,0 +1,13 @@
+/* 62. Unique Paths */
+/* recursive method, Time Limit Exceeded */
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+}
