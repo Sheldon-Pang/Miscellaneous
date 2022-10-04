ed1e7b82530acd53e48b88eff865dcc0637b0f10
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-10-04 14:24:55 -0700

    167. Two Sum II - Input Array Is Sorted

---
+++ Everything_ Data_Structures_And_Algorithms/Blind75/2_Two_Pointers/167_Two_Sum_II_Input_Array_Is_Sorted/two_pointer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+
+            int curSum = numbers[left] + numbers[right];
+
+            if (curSum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (curSum > target) {
+                right--;
+            } else {
+                left++;
+            }
+
+        }
+
+        return new int[] {-1, -1};
+    }
+}
