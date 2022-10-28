4ac85607491fd9be016c83372187e225fa80ecfe
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-10-28 03:41:30 -0700

    1046. Last Stone Weight

---
+++ Everything_ Data_Structures_And_Algorithms/Blind75/9_Heap_Priority_Queue/1046_Last_Stone_Weight/max_heap.java
@@ -0,0 +1,24 @@
+/*
+
+Time: O(n * long(n))
+Space: O(n)
+
+*/
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int stone : stones) {
+            heap.add(stone);
+        }
+
+        while (heap.size() > 1) {
+            int stone1 = heap.remove();
+            int stone2 = heap.remove();
+            if (stone1 != stone2) {
+                heap.add(stone1 - stone2);
+            }
+        }
+
+        return heap.isEmpty() ? 0 : heap.remove();
+    }
+}
